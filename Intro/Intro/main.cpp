aca994d3215ae06a5e2d14466fcdfe3c70af63f1
Clayman <Clayman@DESKTOP-DM8E77U>
2021-04-03 19:27:17 +0300

    Operators overloading

--- Intro/Intro/main.cpp
+++ Intro/Intro/main.cpp
@@ -22,11 +22,17 @@ public:
 	}
 	void set_x(double x)
 	{
-		this->x = x;
+		if (x <= 100)
+			this->x = x;
+		else
+			this->x = 100;
 	}
 	void set_y(double y)
 	{
-		this->y = y;
+		if (y <= 100)
+			this->y = y;
+		else
+			this->y = 100;
 	}
 
 	//			Constructors:
@@ -60,11 +66,43 @@ public:
 	}
 
 	//			Operators:
-	void operator=(const Point& other)
+	Point& operator=(const Point& other)
 	{
 		this->x = other.x;
 		this->y = other.y;
 		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+	Point& operator+=(const Point& other)
+	{
+		/*this->x += other.x;
+		this->y += other.y;*/
+		this->set_x(this->x + other.x);
+		this->set_y(this->y + other.y);
+		return *this;
+	}
+
+	//			Increment/Decrement
+	Point& operator++()	//Prefix increment
+	{
+		++this->x;
+		++this->y;
+		return *this;
+	}
+	Point operator++(int)	//Postfix increment
+	{
+		Point old = *this;
+		/*this->x++;
+		this->y++;*/
+		++*this;
+		return old;
+	}
+
+	Point& operator()(double x, double y)
+	{
+		set_x(x);
+		set_y(y);
+		return *this;
 	}
 
 	//			Methods:
@@ -72,17 +110,56 @@ public:
 	{
 		cout << "X = " << x << tab << "Y = " << y << endl;
 	}
+
+	//			Friends:
+	friend ostream& operator<<(ostream& os, const Point& obj);
+	friend istream& operator>>(istream& is, Point& obj);
 };
 
-void function()
+Point operator+(const Point& left, const Point& right)
 {
-	//this;
+	Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;
+}
+
+bool operator==(const Point& left, const Point& right)
+{
+	/*if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+		return true;
+	else
+		return false;*/
+	return left.get_x() == right.get_x() && left.get_y() == right.get_y();
+}
+bool operator!=(const Point& left, const Point& right)
+{
+	return !(left == right);
+}
+
+ostream& operator<<(ostream& os, const Point& obj)
+{
+	//os << "X = " << obj.get_x() << tab << "Y = " << obj.get_y();
+	os << "X = " << obj.x << tab << "Y = " << obj.y;
+	return os;
+}
+istream& operator>>(istream& is, Point& obj)
+{
+	/*double x, y;
+	is >> x >> y;
+	obj.set_x(x);
+	obj.set_y(y);*/
+	is >> obj.x >> obj.y;
+	return is;
 }
 
 //Структура - это тип данных
 //Класс - это тип данных
 
 //#define STRUCT
+//#define CONSTRUCTORS_CHECK
+//#define ASSIGNMENT_CHECK
+//#define STREAMS
 
 void main()
 {
@@ -102,10 +179,11 @@ void main()
 	cout << (*pA).x << tab << (*pA).y << endl;
 #endif // STRUCT
 
+#ifdef CONSTRUCTORS_CHECK
 	Point A;	//Default constructor
-	/*A.set_x(2);
-	A.set_y(3);*/
-	//cout << A.get_x() << tab << A.get_y() << endl;
+/*A.set_x(2);
+A.set_y(3);*/
+//cout << A.get_x() << tab << A.get_y() << endl;
 	A.print();
 
 	Point B(2, 3);
@@ -120,6 +198,63 @@ void main()
 	Point E;	//Default constructor
 	E = D;		//Copy assignment
 	E.print();
+#endif // CONSTRUCTORS_CHECK
+
+#ifdef ASSIGNMENT_CHECK
+	int a, b, c;
+	a = b = c = 0;
+
+	Point A, B, C;
+	A = B = C = Point(2, 3);
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT_CHECK
+
+#ifdef STREAMS
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+
+	Point A(2, 3);
+	Point B(3, 4);
+	//A.print();
+	//B.print();
+	/*Point C = A + B;
+	C.print();
+	A.print();
+	B.print();*/
+	//A += B;
+	//A.print();
+	/*Point D = ++A;
+	A.print();
+	D.print();*/
+
+	cout << A << endl;
+	//	(ostream)	(Point)
+	cout << B << endl;
+	cout << "Введите координаты точки: ";
+	cin >> A;
+	cout << "Вы ввели: " << A << endl;
+#endif // STREAMS
+
+	Point A(2, 3);
+	Point B(3, 4);
+	/*if (A == A)
+	{
+		cout << "Точки равны" << endl;
+	}
+	else
+	{
+		cout << "Точки разные" << endl;
+	}*/
+	cout << (A != B) << endl;
+
+	cout << A << endl;
+	/*A.set_x(33);
+	A.set_y(44);*/
+	A(33, 44);
+	cout << A << endl;
 }
 
 /*
@@ -162,5 +297,25 @@ Constructor - это метод, который создает объект;
 			  по истечении его времени жизни;
 			  ~ - tilda;
 Operator=;
+-----------------------------------------------------
+*/
+
+/*			OPERATORS OVERLOADING RULES
+-----------------------------------------------------
+1. Перегрузить можно только существующие операторы
+	+  - перегружается;
+	++ - перегружается;
+	*  - перегружается;
+	** - НЕ перегружается;
+2. НЕ все существующие операторы можно перегрузить.
+   НЕ перегружаются:
+	?: - Ternary;
+	:: - Scope operator (Оператор разрешения видимости);
+	.  - Point operator (Оператор прямого доступа);
+	.* - Pointer to member selection;
+	#
+	##
+3. Перегруженные операторы сохраняют приоритет;
+4. int + int
 -----------------------------------------------------
 */
